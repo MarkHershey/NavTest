b2e2d55892cce9300ec4b86806873cd1051ec655
Mark Huang <hhzeos@gmail.com>
2020-11-22 17:29:39 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/mynavtest/ui/activities/ActivitiesViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mynavtest.ui.activities;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ActivitiesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ActivitiesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Activities fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
